c31922d6cae86f43c22005757249d6ae0ac841ea
vadimssssss <108651155+vadimssssss@users.noreply.github.com>
2022-08-09 15:31:18 +0300

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const sticker = document.querySelector('.sticker');
-
-window.onscroll = function () {
-    if (window.pageYOffset > 76) {
-        sticker.classList.add('active');
-    } else {
-        sticker.classList.remove('active');
-    };
-};
