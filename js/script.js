554f3e87c2bff694a940ac1d07ede87436bd1325
vadimssssss <108651155+vadimssssss@users.noreply.github.com>
2022-09-15 19:20:07 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
-$('.rewiew__slider').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    arrows: false
+// $('.rewiew__slider').slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 300,
+//     slidesToShow: 1,
+//     arrows: false,
+//     centerMode: true, 
+//     // centerPadding: '150px'
+//     mobileFirst: true,
+//     variableWidth: true
+// });
+
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      header = document.querySelector('.header');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger__active');
+    menu.classList.toggle('menu__active');
+    header.classList.toggle('header__fixed');
 });
